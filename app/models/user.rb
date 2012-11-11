e3cf44b4692d36bd470963be187495e065993a7c
mannyFresh <marcs@ccs.neu.edu>
2012-11-11 14:34:29 -0500

    Initial checkin

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ActiveRecord::Base
+  attr_accessible :age, :name
+  validates :age, :name, :presence => true
+  validates :age, :numericality => { :greater_than => 21 }
+  validates :name, :uniqueness => true
+  # validates :name, :uniqueness => { :message => 'someone already took' }
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UserApp::Application.config.secret_token = '12e6dbbdaa96244e1bc4a641dfa677366a1a33ea1ebe982ca000221480998922d0b003789adf170d70026f9ca9212f00055aa4166fda5ef869eb3e494b181683'
